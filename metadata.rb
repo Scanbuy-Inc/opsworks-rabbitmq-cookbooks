2526a081bf5a6d81c2ffa80321e134e4ca43212a
Sean Zou <sean.zou@scanbuy.com>
2014-09-09 16:18:23 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,11 @@ description 'Example cookbook for RabbitMQ cluster'
 maintainer  "Sean Zou"
 license     "Apache 2.0"
 version     "1.0.0"
+
+%w{ubuntu debian linuxmint redhat centos scientific amazon fedora oracle smartos suse}.each do |os|
+  supports os
+end
+
+%w{ rabbitmq hostsfile }.each do |ckbk|
+  depends ckbk
+end
